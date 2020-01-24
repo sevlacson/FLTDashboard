fe32c7a17c6d25a60311d2f1bc5f1b66d8ccab15
sevlacson <ms.lacson@torrestech.ph>
2020-01-24 09:00:17 +0800

    Add more data labels

--- WebContent/js/demo/chart-bar-demo.js
+++ WebContent/js/demo/chart-bar-demo.js
@@ -16,6 +16,16 @@ new Chart(document.getElementById("efficiency-chart"), {
           backgroundColor: ["rgba(31, 135, 255, 1)","#dc3545","rgba(31, 135, 255, 1)","#dc3545", "#dc3545", "rgba(31, 135, 255, 1)", "#dc3545"], 
           barPercentage: 1,
           data: [101,74,108,87,86,107,86],
+          datalabels: {
+            formatter: (value, ctx) => {
+            
+              let percentage = (value)+"%";
+              return percentage;
+      
+            },
+            color: '#fff',
+            display: true
+                 }
         },
       ]
     },
@@ -158,12 +168,22 @@ yAxisID: 'y-axis-1'
         type: "bar",
         backgroundColor: ["rgba(31, 135, 255, 1)", "#dc3545", "rgba(31, 135, 255, 1)", "rgba(31, 135, 255, 1)", "rgba(31, 135, 255, 1)", "rgba(31, 135, 255, 1)", "rgba(31, 135, 255, 1)", "rgba(31, 135, 255, 1)"],
         data: [23.3,1.3,9.6,6.8,6,9.9,4.9,5],
+        datalabels: {
+          // display labels for this specific dataset
+          display: true,
+          color: '#fff'
+        },
 yAxisID: 'y-axis-2'
       }, {
         label: "Actual Sales",
         type: "bar",
         backgroundColor: ["#529dee", "#d85562", "#529dee", "#529dee", "#529dee", "#529dee", "#529dee", "#529dee"],
         data: [22.5,2,11.5,8,9.9,8.7,5,4],
+        datalabels: {
+          // display labels for this specific dataset
+          display: true,
+          color: '#fff'
+        },
 yAxisID: 'y-axis-2'
       },
     ]
